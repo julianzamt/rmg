ba3ab60cc01dea66b0431ca480ac379fbe14da93
Julian Zamt <julizamt@gmail.com>
2021-02-01 21:32:12 -0300

    fixed width

--- src/Meme.js
+++ src/Meme.js
@@ -3,7 +3,7 @@ import React from "react"
 function Meme(props){
     return(
         <div className="meme">
-                <img style={{maxWidth: 300}} src={props.random_img}></img>
+                <img style={{maxWidth: 500}} src={props.random_img}></img>
                 <h2 className="top">{props.top_text}</h2>
                 <h2 className="bottom">{props.bottom_text}</h2>
             </div>
